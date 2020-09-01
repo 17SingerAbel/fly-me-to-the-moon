f1464569b83b5bdc952921ea3c054484cfdff567
Yeqi Sang <abelsang9683@gmail.com>
2020-09-01 19:53:36 -0400

    nicxe

---
+++ Assets/ContinuousMovement.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+using UnityEngine.XR.Interaction.Toolkit;
+public class ContinuousMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public XRNode inputSource;
+    public float gravity = 9.8f;
+    public float speed = 1f;
+    public float additionalHeight = 0.2f;
+    private Vector2 inputAxis;
+    private XRRig rig;
+    private CharacterController character;
+    private float fallingSpeed;
+    void Start()
+    {
+        character = GetComponent<CharacterController>();
+        rig = GetComponent<XRRig>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        InputDevice device = InputDevices.GetDeviceAtXRNode(inputSource);
+        device.TryGetFeatureValue(CommonUsages.primary2DAxis, out inputAxis);
+
+    }
+
+    private void FixedUpdate()
+    {
+        CapsuleFollowHeadset();
+        Quaternion headYaw = Quaternion.Euler(0, rig.cameraGameObject.transform.eulerAngles.y, 0);
+
+        Vector3 direction = headYaw * new Vector3(inputAxis.x, 0, inputAxis.y);
+        character.Move(direction * Time.fixedDeltaTime * speed);
+
+        if (!character.isGrounded)
+        {
+            //gravity
+            fallingSpeed -= gravity * Time.fixedDeltaTime;
+        }
+        else
+        {
+            fallingSpeed = 0.0f;
+        }
+        character.Move(Vector3.up * fallingSpeed * Time.fixedDeltaTime);
+    }
+
+    void CapsuleFollowHeadset()
+    {
+        character.height = rig.cameraInRigSpaceHeight + additionalHeight;
+        Vector3 capsuleCenter = transform.InverseTransformPoint(rig.cameraGameObject.transform.position);
+        character.center = new Vector3(capsuleCenter.x, character.height / 2 + character.skinWidth, capsuleCenter.z);
+    }
+}
