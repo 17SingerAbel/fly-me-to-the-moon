3f5be457300033e4c8da9dc5b89c416d332347c7
Yeqi Sang <abelsang9683@gmail.com>
2020-09-01 09:29:02 -0400

    succesS

--- Assets/Scripts/SelfMouseLook.cs
+++ Assets/Scripts/SelfMouseLook.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MouseLook : MonoBehaviour
+public class SelfMouseLook : MonoBehaviour
 {
     public float mouseSensitivity = 100f;
     public Transform body;
